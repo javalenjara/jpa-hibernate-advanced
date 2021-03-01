5230d22d1950e8cbfa991b40be979a37fceb5bd1
Jorge Andres Valencia Jaramillo <Jvalencia@intergrupo.com>
2021-02-28 19:39:19 -0500

    Initial commit

---
+++ src/main/java/com/jpa/hibernate/demojpaadvanced/DemoJpaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.hibernate.demojpaadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJpaAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpa/hibernate/demojpaadvanced/DemoJpaAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.hibernate.demojpaadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
