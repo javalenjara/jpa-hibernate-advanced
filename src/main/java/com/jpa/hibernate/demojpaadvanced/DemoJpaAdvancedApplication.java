ce5820faf8b87922ed64900f3d6e931a34ab1f2d
javalenja <javalenja@gmail.com>
2021-04-24 17:40:26 -0500

    Addding class inheritances
    Using @Inheritance and @DiscriminatorColumn to map inheritance between classes

--- src/main/java/com/jpa/hibernate/demojpaadvanced/DemoJpaAdvancedApplication.java
+++ src/main/java/com/jpa/hibernate/demojpaadvanced/DemoJpaAdvancedApplication.java
@@ -1,9 +1,8 @@
 package com.jpa.hibernate.demojpaadvanced;
 
-import com.jpa.hibernate.demojpaadvanced.entities.Course;
-import com.jpa.hibernate.demojpaadvanced.entities.Review;
-import com.jpa.hibernate.demojpaadvanced.entities.Student;
+import com.jpa.hibernate.demojpaadvanced.entities.*;
 import com.jpa.hibernate.demojpaadvanced.repository.CourseRepository;
+import com.jpa.hibernate.demojpaadvanced.repository.EmployeeRepository;
 import com.jpa.hibernate.demojpaadvanced.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +22,14 @@ public class DemoJpaAdvancedApplication implements CommandLineRunner {
 
     private final CourseRepository courseRepository;
     private final StudentRepository studentRepository;
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public DemoJpaAdvancedApplication(CourseRepository courseRepository, StudentRepository studentRepository) {
+    public DemoJpaAdvancedApplication(CourseRepository courseRepository, StudentRepository studentRepository,
+                                      EmployeeRepository employeeRepository) {
         this.courseRepository = courseRepository;
         this.studentRepository = studentRepository;
+        this.employeeRepository = employeeRepository;
     }
 
     public static void main(String[] args) {
@@ -41,8 +44,15 @@ public class DemoJpaAdvancedApplication implements CommandLineRunner {
 //        reviews.add(new Review("5", "Hats off"));
 //        courseRepository.addReviewsForCourse(1003L, reviews);
         //studentRepository.insertStudentAndCourse();
-        studentRepository.insertStudentAndCourse(
-                new Student("Jack"),
-                new Course("Microservices"));
+//        studentRepository.insertStudentAndCourse(
+//                new Student("Jack"),
+//                new Course("Microservices"));
+        FullTimeEmployee fte = new FullTimeEmployee("Jack", new BigDecimal("10000"));
+        PartTimeEmployee pte = new PartTimeEmployee("Jill", new BigDecimal("50"));
+        employeeRepository.insert(pte);
+        employeeRepository.insert(fte);
+
+        logger.info("All Employees -> {}", employeeRepository.retrieveAllEmployees());
+
     }
 }
--- src/main/java/com/jpa/hibernate/demojpaadvanced/repository/EmployeeRepository.java
+++ src/main/java/com/jpa/hibernate/demojpaadvanced/repository/EmployeeRepository.java
@@ -25,74 +25,8 @@ public class EmployeeRepository {
         em.persist(employee);
     }
 
-    public Course retrieveAllEmployees(){
-        return null;
-    }
-
-    public Course findById(Long id){
-        return em.find(Course.class, id);
-    }
-
-    public Course save(Course course){
-        if (course.getId() == null){
-            em.persist(course);
-        }
-        else {
-            em.merge(course);
-        }
-        return course;
-    }
-
-    public void deleteById(Long id){
-        Course course = findById(id);
-        em.remove(course);
-    }
-
-    public void playWithEntityManager() {
-        Course course1 = new Course("Web Services part 2");
-        em.persist(course1);
-
-        Course course2 = findById(1001L);
-        course2.setName("JPA course - updated");
-    }
-
-    //static approach
-    /*public void addReviewsForCourse() {
-        //get te course 1003
-        Logger logger = LoggerFactory.getLogger(this.getClass());
-        Course course = findById(1003L);
-        logger.info("course.getReviews() -> {}", course.getReviews());
-
-        //add 2 reviews to it
-        Review review1 = new Review("5", "Great Hands-on");
-        Review review2 = new Review("5", "Hatsoff");
-
-        //creating the relationship
-        course.addReview(review1);
-        review1.setCourse(course);
-
-        course.addReview(review2);
-        review2.setCourse(course);
-
-        //save it to the DB
-        em.persist(review1);
-        em.persist(review2);
-    }*/
-
-    //More generic addReviewsForCourse method
-    public void addReviewsForCourse(Long courseId, List<Review> reviews) {
-
-        Course course = findById(courseId);
-        reviews.stream().forEach(review -> {
-            course.addReview(review);
-            review.setCourse(course);
-            em.persist(review);
-        });
-
-        /*for (Review review : reviews) {
-            course.addReview(review);
-            review.setCourse(course);
-            em.persist(review);
-        }*/
+    public List<Employee> retrieveAllEmployees(){
+        return em.createQuery("SELECT e FROM Employee e", Employee.class)
+                .getResultList();
     }
 }
