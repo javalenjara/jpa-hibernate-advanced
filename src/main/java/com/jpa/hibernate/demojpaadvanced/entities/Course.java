de6cb06580d38f508e2b907ec756916d2502d4f4
javalenja <javalenja@gmail.com>
2021-06-06 15:47:31 -0500

    Adding Spring Data Rest.

--- src/main/java/com/jpa/hibernate/demojpaadvanced/entities/Course.java
+++ src/main/java/com/jpa/hibernate/demojpaadvanced/entities/Course.java
@@ -1,5 +1,6 @@
 package com.jpa.hibernate.demojpaadvanced.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -27,6 +28,7 @@ public class Course {
     private List<Review> reviews = new ArrayList<>();
 
     @ManyToMany(mappedBy = "courses")
+    @JsonIgnore
     private List<Student> students = new ArrayList<>();
 
     @UpdateTimestamp
--- src/main/java/com/jpa/hibernate/demojpaadvanced/repository/CourseSpringDataRepository.java
+++ src/main/java/com/jpa/hibernate/demojpaadvanced/repository/CourseSpringDataRepository.java
@@ -3,9 +3,11 @@ package com.jpa.hibernate.demojpaadvanced.repository;
 import com.jpa.hibernate.demojpaadvanced.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "courses")
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
 
     List<Course> findByName(String name);
