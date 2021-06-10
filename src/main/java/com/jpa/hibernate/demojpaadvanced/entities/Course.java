ca9c4f92d3f6f62499fd64f1046120d930ae5377
javalenja <javalenja@gmail.com>
2021-06-09 22:49:38 -0500

    Soft deletion notes to understand the use of @Where in native queries and the @PreRemove annotation.

--- src/main/java/com/jpa/hibernate/demojpaadvanced/entities/Course.java
+++ src/main/java/com/jpa/hibernate/demojpaadvanced/entities/Course.java
@@ -5,6 +5,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -20,6 +22,8 @@ import java.util.List;
 @SQLDelete(sql = "update course set is_deleted = true where id = ?")
 @Where(clause = "is_deleted = false")
 public class Course {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(Course.class);
 
     @Id
     @GeneratedValue
@@ -42,6 +46,12 @@ public class Course {
     private LocalDateTime createdDate;
 
     private boolean isDeleted;
+
+    @PreRemove
+    private void preRemove() {
+        LOGGER.info("Setting isDeleted to true.");
+        this.isDeleted = true;
+    }
 
     public Course() {
     }
